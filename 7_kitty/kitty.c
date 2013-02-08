f16e3765477ff43563eeca3ae5b771636e26f5a8
Karolin Varner <karo@cupdev.net>
2013-02-08 10:09:43 +0100

    Make kitty love aa

--- 7_kitty/kitty.c
+++ 7_kitty/kitty.c
@@ -3,6 +3,8 @@
 int main() {
     char c;
     while ((c = getchar()) != EOF) {
+        if (c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            c = 'a';
         putc(c, stdout);
     }
     return 0;
