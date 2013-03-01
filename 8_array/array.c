caf399840e6efa0a2ad075f11c5aab9cc93ee03e
Karolin Varner <karo@cupdev.net>
2013-03-01 09:13:54 +0100

    Implement array excercies

--- 8_array/array.c
+++ 8_array/array.c
@@ -1,16 +1,24 @@
 #include "../stdfuns.h"
 
+void dump_i_arr(int[] i)
+    size_t size = sizeof(i);
+    size_t len  = (size / sizeof(int));
+
+    printf("int[] where: len=%d, size=%dbytes: ", len, size);
+    
+    itr(i, len) {
+        puti(a[i]):
+        mputc(' '); 
+    }
+    mputc('\n');
+}
+
+void autoarr() {
+    int a[] = {-2, 4, 4, 122, 42};
+    const size_t len = (sizeof(a) / sizeof(int));
+
+}
+
 int main() {
-    const size_t alen;
-    double d[len];
-
-    for (double i = 0; i<alen; i++)
-        d[i] = i;
-
-    for (double i = 0; i<alen; i++)
-        printf("d[%i] = %i", i, d[i]);
-
-    size_t len
-
     return 0;
 }
