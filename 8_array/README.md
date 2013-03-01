af183e3786588997220b864d07103bce88838664
Karolin Varner <karo@cupdev.net>
2013-03-01 10:15:28 +0100

    Finish all the excercises

--- 8_array/README.md
+++ 8_array/README.md
@@ -6,5 +6,6 @@ First steps with arrays:
 * Initialize an array with custom values (curly braces notation)
 * Display the array
 * Display the size and the number of elements
+* Set ind={0,1,3} to 42
 * Save element #4 (index=4) to a var "d4"
 * Create an array with elements entered by the user
--- 8_array/array.c
+++ 8_array/array.c
@@ -1,24 +1,50 @@
 #include "../stdfuns.h"
 
-void dump_i_arr(int[] i)
-    size_t size = sizeof(i);
-    size_t len  = (size / sizeof(int));
+void dump_i_arr(int *a, size_t len) {
+    size_t size = sizeof(int) * len;
 
-    printf("int[] where: len=%d, size=%dbytes: ", len, size);
+    printf("int[] where: len=%ld, size=%ldbytes: ", len, size);
     
     itr(i, len) {
-        puti(a[i]):
+        puti(a[i]);
         mputc(' '); 
     }
     mputc('\n');
 }
 
 void autoarr() {
+    // (1) INIT
     int a[] = {-2, 4, 4, 122, 42};
-    const size_t len = (sizeof(a) / sizeof(int));
+    size_t len = sizeof(a) / sizeof(int);
 
+    // PRINT
+    dump_i_arr(a, len);
+
+    // SET 42
+    a[0] = a[1] = a[3] = 42;
+    dump_i_arr(a, len);
+
+    // GET d4
+    int d4 = a[4];
+    printf("d4 = a[4] = %i\n\n", d4);
+}
+
+void userarr() {
+    mgeti("Create array/len: ", len);
+
+    int arr[len];
+    itr(i, len) {
+        printf("Create array/%d: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    dump_i_arr(arr, len);
 }
 
 int main() {
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    autoarr();
+    userarr();
     return 0;
 }
