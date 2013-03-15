2b3af4b499a983d27d27fc5238c368920c076d75
Karolin Varner <karo@cupdev.net>
2013-03-15 08:34:10 +0100

    Use small numbers: randarng

--- 8_sort/sort.c
+++ 8_sort/sort.c
@@ -26,12 +26,14 @@ void bubblesort(int* ar, size_t len) {
 ///////////////////////////////////////////////
 // Main
 
-#define sortt_l(f, len)        \
-    size_t ar_l = len;      \
-    int* ar = randar(ar_l); \
-    dump_i_arr(ar,ar_l);    \
-    f(ar, ar_l);   \
+#define sortt_lr(f, a, z, len)        \
+    size_t ar_l = len;                \
+    int* ar = randarng(a, z, ar_l);   \
+    dump_i_arr(ar,ar_l);              \
+    f(ar, ar_l);                      \
     dump_i_arr(ar, ar_l);
+
+#define sortt_l(f, len) sortt_lr(f, 0, len*2, len)
 
 #define sortt(f) sortt_l(f, 16)
 
